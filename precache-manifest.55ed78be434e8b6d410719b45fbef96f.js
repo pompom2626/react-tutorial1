37ffe4b1f0ebb31cda34602aaaee28fd5cef120b
pompom2626 <pompom2626@gmail.com>
2020-01-14 01:25:31 -0600

    Updates

---
+++ precache-manifest.55ed78be434e8b6d410719b45fbef96f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b225d9879aa6a1719ef565def3bc442b",
+    "url": "/react-tutorial1/index.html"
+  },
+  {
+    "revision": "28ab4d32a7e9c3c80094",
+    "url": "/react-tutorial1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "28f88d6e7bf2a5d74419",
+    "url": "/react-tutorial1/static/js/2.d35a2c42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial1/static/js/2.d35a2c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28ab4d32a7e9c3c80094",
+    "url": "/react-tutorial1/static/js/main.634f0077.chunk.js"
+  },
+  {
+    "revision": "80fff669c24f90647b18",
+    "url": "/react-tutorial1/static/js/runtime-main.298a0f5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial1/static/media/logo.5d5d9eef.svg"
+  }
+]);
